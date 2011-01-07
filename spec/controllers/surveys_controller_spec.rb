de4dd93fe2d8da5f55b66011699fa07c061ea787
Aaron Peckham <apeckham@gmail.com>
2011-01-06 16:31:13 -0800

    use rspec

---
+++ spec/controllers/surveys_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe SurveysController do
+
+  #Delete this example and add some real ones
+  it "should use SurveysController" do
+    controller.should be_an_instance_of(SurveysController)
+  end
+
+end
