3b621244da10d364e217eba6157efb9ef1a868df
Pivotal <pivotal@emo.local>
2011-02-22 21:13:42 -0800

    genercize form validation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,18 @@
 function liveValidateForm ($form) {
   function formCompleted () {
-    return $('#survey_what').val().length > 0 && $('#survey_description').val().length > 0;
+    // return $('#survey_what').val().length > 0 && $('#survey_description').val().length > 0;
+    var allCompleted = true;
+    $form.find('input[type=text], textarea').each(function () {
+      if ($(this).val().length == 0) {
+        allCompleted = false;
+      }
+    });
+    
+    return allCompleted;
   }
 
   $("input, textarea").keyup(function() {
-    $("#survey_submit").attr("disabled", !formCompleted());
+    $form.find('input[type=submit]').attr("disabled", !formCompleted());
   });
 
   $form.submit(function () {
