15a699f63b7fda34fc8ce61b62e77d1ddf6da1c0
Pivotal <pivotal@emo.local>
2011-02-22 20:59:53 -0800

    add test

--- spec/javascripts/application-spec.js
+++ spec/javascripts/application-spec.js
@@ -5,7 +5,16 @@ describe("liveValidateForm", function() {
     expect($('form input[type=submit]').attr('disabled')).toBeFalsy();
     $('form input').keyup();
     expect($('form input[type=submit]').attr('disabled')).toBeTruthy();
+    
+    $("#survey_what").val("WHAT!!");
+    $('form input').keyup();
+    expect($('form input[type=submit]').attr('disabled')).toBeTruthy();
+    $("#survey_description").val("description");
+    $('form input').keyup();
+    expect($('form input[type=submit]').attr('disabled')).toBeFalsy();
   });
   
-  it("prevents the form from being submitted until all fields are non empty");
+  it("prevents the form from being submitted until all fields are non empty", function() {
+    
+  });
 });
