1fc60929a1659768dde9f94fb18dd720c772586d
Erik Hanson <erik@eahanson.com>
2011-03-07 19:58:26 -0800

    started to make request for domai.nr

--- public/javascripts/namer.js
+++ public/javascripts/namer.js
@@ -1,7 +1,20 @@
 var namer = {
   lookUpDomains: function() {
     $('#suggestions .name').each(function(i, obj) {
-//      $.ajax();
+      namer.lookup(name);
     });
+  },
+
+  lookup: function(name, resultElement) {
+    $.ajax({ success: function(response) {
+      var formatted = "";
+      $(response.results).each(function() {
+        formatted += this.domain;
+        formatted += ": ";
+        formatted += this.availability;
+        formatted += "\r";
+      });
+      $(resultElement).find(".domains").attr("title", formatted);
+    }});
   }
 };
--- spec/javascripts/namer-spec.js
+++ spec/javascripts/namer-spec.js
@@ -1,13 +1,18 @@
 describe("namer", function() {
   describe("lookUpDomains", function() {
-    beforeEach(function() {
+    it("should populate suggestion's domain element", function() {
+      var suggestionElement = $("<div class='suggestion'><div class='domains'></div></div>")
+      expect($(".suggestion .domains").text(), suggestionElement).toEqual("");
 
-      spyOn($, 'ajax');
-      spec.loadFixture('show');
-    });
-
-    it("should make a request for each suggestion", function() {
-//      expect($.ajax.callCount).toEqual(3);
+      var ajax = { "query": "domai.nr", "results": [
+        { "domain": "domai.nr", "register_url": "http://domai.nr/domai.nr/register", "host": "", "path": "", "subdomain": "domai.nr", "availability": "taken" },
+        { "domain": "dom.ai", "register_url": "http://domai.nr/dom.ai/register", "host": "", "path": "/nr", "subdomain": "dom.ai", "availability": "available" }
+      ]};
+      spyOn($, "ajax").andCallFake(function(options) {
+        options.success(ajax);
+      });
+      namer.lookup("domai.nr", suggestionElement);
+      expect($(suggestionElement).find(".domains").attr("title")).toEqual("domai.nr: taken\rdom.ai: available\r");
     });
   });
 });
